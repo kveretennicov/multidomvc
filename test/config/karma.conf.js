874206c4bbb4e9653a7cb823320891efb11511bd
Konstantin Veretennicov <kveretennicov@gmail.com>
2017-01-28 19:43:48 +0100

    refactor: use local copy of Angular and TodoMVC stylesheet

--- test/config/karma.conf.js
+++ test/config/karma.conf.js
@@ -5,9 +5,9 @@ module.exports = function (config) {
 		basePath: '../../',
 		frameworks: ['jasmine'],
 		files: [
-			'node_modules/angular/angular.js',
-			'node_modules/angular-route/angular-route.js',
-			'node_modules/angular-resource/angular-resource.js',
+			'lib/angular.js',
+			'lib/angular-route.js',
+			'lib/angular-resource.js',
 			'node_modules/angular-mocks/angular-mocks.js',
 			'js/**/*.js',
 			'test/unit/**/*.js'
