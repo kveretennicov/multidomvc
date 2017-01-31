da346318124dc470e7bd0c8f6db285e163d6a6ba
Konstantin Veretennicov <kveretennicov@gmail.com>
2017-01-31 19:30:47 +0100

    feat(revisioning): add Reversion support to backend admin

--- backend/todomvc/backend/admin.py
+++ backend/todomvc/backend/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
 from . import models
 
@@ -7,9 +8,13 @@ class ToDoItemInline(admin.TabularInline):
     model = models.ToDoItem
 
 
-class ToDoListAdmin(admin.ModelAdmin):
+class ToDoListAdmin(VersionAdmin):
     inlines = [ToDoItemInline]
 
 
+class ToDoItemAdmin(VersionAdmin):
+    model = models.ToDoItem
+
+
 admin.site.register(models.ToDoList, ToDoListAdmin)
-admin.site.register(models.ToDoItem)
+admin.site.register(models.ToDoItem, ToDoItemAdmin)
